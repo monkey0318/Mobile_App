476d91b2954616787c0c0fb9a96650d3e5acc9e5
Yong Sen <yongsen318@gmail.com>
2023-12-26 22:26:23 +0800

    nutrition activity ( design not complete )

--- app/src/main/java/com/example/mobile_app/NutritionTracking.kt
+++ app/src/main/java/com/example/mobile_app/NutritionTracking.kt
@@ -8,13 +8,33 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mobile_app.databinding.ActivityNutritionTrackingBinding
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 class NutritionTracking : AppCompatActivity() {
 
     private lateinit var binding: ActivityNutritionTrackingBinding
 
+    lateinit var carbInput: EditText
+    lateinit var proteinInput: EditText
+    lateinit var fatInput: EditText
+
+    lateinit var fat_TV: TextView
+    lateinit var carb_TV: TextView
+    lateinit var protein_TV: TextView
+    lateinit var totalProtein: TextView
+    lateinit var totalFat: TextView
+    lateinit var totalCarb: TextView
+
+    lateinit var addNutritionsButton: Button
+
+    private var totalProteinValue = 0f
+    private var totalFatValue = 0f
+    private var totalCarbValue = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityNutritionTrackingBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,5 +51,48 @@ class NutritionTracking : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        proteinInput = findViewById(R.id.proteinInput)
+        carbInput = findViewById(R.id.carbInput)
+        fatInput = findViewById(R.id.fatInput)
+
+        fat_TV = findViewById(R.id.fat_TV)
+        carb_TV = findViewById(R.id.carb_TV)
+        protein_TV = findViewById(R.id.protein_TV)
+        totalProtein = findViewById(R.id.totalProtein)
+        totalFat = findViewById(R.id.totalFat)
+        totalCarb = findViewById(R.id.totalCarb)
+
+        addNutritionsButton = findViewById(R.id.addNutrientsButton)
+
+        addNutritionsButton.setOnClickListener{
+            addNutrients()
+        }
+
+    }
+
+    private fun addNutrients() {
+        val proteinValue = proteinInput.text.toString().toFloatOrNull() ?: 0f
+        val carbValue = carbInput.text.toString().toFloatOrNull() ?: 0f
+        val fatValue = fatInput.text.toString().toFloatOrNull() ?: 0f
+
+        totalProteinValue += proteinValue
+        totalCarbValue += carbValue
+        totalFatValue += fatValue
+
+        updateTotalNutrients()
+        clearInputFields()
+    }
+
+    private fun updateTotalNutrients() {
+        totalProtein.text = "Total Protein: $totalProteinValue g"
+        totalCarb.text = "Total Carbs: $totalCarbValue g"
+        totalFat.text = "Total Fat: $totalFatValue g"
+    }
+
+    private fun clearInputFields() {
+        proteinInput.text.clear()
+        carbInput.text.clear()
+        fatInput.text.clear()
     }
 }
