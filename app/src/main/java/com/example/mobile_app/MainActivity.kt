c12d8ea9d0f225d668bf6b51908a8ac4c5211d7e
HISOKA_AN <qfengqin@gmail.com>
2023-12-28 02:48:07 +0800

    change login and register

--- app/src/main/java/com/example/mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -4,15 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    //private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        //FirebaseApp.initializeApp(this)
         setContentView(R.layout.activity_login)
+        val emailLogin:EditText = findViewById(R.id.editEmailLoginText)
+        val passwordLogin:EditText = findViewById(R.id.editPasswordLoginText)
+        val loginBtn:ImageView = findViewById(R.id.login_arrow)
         val registerTextView:TextView= findViewById(R.id.registerTextView)
+
+        loginBtn.setOnClickListener { loginCheckSuccess(it,emailLogin.text.toString(), passwordLogin.text.toString()) }
         registerTextView.setOnClickListener { registerAccount(it) }
     }
+
+    private fun loginCheckSuccess(view: View, email: String, password: String) {
+       // auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this)
+       // { task: Task<AuthResult> ->
+           // if (task.isSuccessful) {
+                val context = view.context
+        //haven't know where to send
+             //   val intent = Intent(context, Register::class.java)
+                context.startActivity(intent)
+           // }
+           //else{
+                Toast.makeText(view.context, "Email/Password wrong", Toast.LENGTH_SHORT).show()
+          //  }
+      //  }
+    }
+
+
     private fun registerAccount(view: View) {
         val context = view.context
         val intent = Intent(context, Register::class.java)
--- app/src/main/java/com/example/mobile_app/Register.kt
+++ app/src/main/java/com/example/mobile_app/Register.kt
@@ -1,7 +1,12 @@
 package com.example.mobile_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Register : AppCompatActivity() {
@@ -9,6 +14,7 @@ class Register : AppCompatActivity() {
     lateinit var editNameText:EditText
     lateinit var editEmailText: EditText
     lateinit var editPasswordText: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +28,41 @@ class Register : AppCompatActivity() {
         val name = editNameText.text.toString()
         val email = editEmailText.text.toString()
         val password = editPasswordText.text.toString()
+        val registerBtn: ImageView = findViewById(R.id.register_arrow)
+        val loginTextView:TextView= findViewById(R.id.loginTextView)
 
-        //store data
+        registerBtn.setOnClickListener { checkEmpty(it) }
+        loginTextView.setOnClickListener { loginAccount(it) }
 
-        //val user=User(name,email,password)
-         //val database = FirebaseDatabase.getInstance()
-        //val usersRef = database.getReference("users")
-        //val userId = usersRef.push().key
-        //usersRef.child(userId!!).setValue(user)
+    }
+
+    private fun checkEmpty(view:View){
+        val name = editNameText.text.toString()
+        val email = editEmailText.text.toString()
+        val password = editPasswordText.text.toString()
+
+        if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            //store data
+
+            //val user= User(name,email,password)
+            //  val database = FirebaseDatabase.getInstance()
+            // val usersRef = database.getReference("users")
+            // val userId = usersRef.push().key
+            // usersRef.child(userId!!).setValue(user)
+
+            loginAccount(view)
+
+        }else {
+
+            Toast.makeText(this, "Do not leave empty", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun loginAccount(view: View) {
+        val context = view.context
+        val intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
     }
 
 }
